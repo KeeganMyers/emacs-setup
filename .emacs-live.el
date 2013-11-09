9bc399d459e3e3a10ff752113855125820dd0b25
EricGebhart <e.a.gebhart@gmail.com>
2013-11-09 16:20:07 -0500

    added .emacs-live.el

---
+++ .emacs-live.el
@@ -0,0 +1,24 @@
+;; a utility function to help in installing emacs-live packs
+(defun add-live-packs (path packs)
+  "Adding live packs at startup"
+  (live-add-packs
+   (mapcar
+    (lambda (pack)
+      (concat path pack))
+    packs)))
+
+;; all the other packs needed
+(add-live-packs "~/.emacs.d/emacs-live-packs/"
+                '(;; installing emacs repositories (melpa, milkbox, marmalade) and the install-packs utility function
+                  "install-packages-pack"
+                  "clojurescript-pack"
+                  "eproject-pack"
+                  ;vi emulation.
+                  "evilmode-pack"
+                  ;automatic tags.
+                  "helm-ctags-pack"
+                  ;emacs window manager.
+                  "e2wm-pack"
+                  ;buffer management and key bindings.
+                  "cyclebufs-pack"
+                  ))
