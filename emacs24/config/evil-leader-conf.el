f8c68302ef9ee2070d418324c303dde5d831b58d
EricGebhart <e.a.gebhart@gmail.com>
2015-03-17 17:10:04 -0400

    Figured out how to create a two level map. Mapped cider key maps.

--- emacs24/config/evil-leader-conf.el
+++ emacs24/config/evil-leader-conf.el
@@ -3,6 +3,8 @@
 (setq evil-leader/in-all-states t)
 (require 'evil-leader)
 
+(require 'cider-doc)
+
 (global-evil-leader-mode)
 
 (defun save-and-load ()
@@ -30,46 +32,87 @@
 ;;   (define-key evil-normal-state-map ",cr" 'comment-or-uncomment-region)
 ;;   (define-key evil-normal-state-map ",cv" 'evilnc-toggle-invert-comment-line-by-line))
 
+(defvar cider-sub-map
+  (let (cider-sub-map)
+    (define-prefix-command 'cider-sub-map)
+    (define-key cider-sub-map (kbd "G") #'cider-grimoire-eww)
+    (define-key cider-sub-map (kbd "n") #'cider-browse-ns)
+    (define-key cider-sub-map (kbd "N") #'cider-browse-ns-all)
+    (define-key cider-sub-map (kbd "u") #'cider-undef)
+    (define-key cider-sub-map (kbd "i") #'cider-inspect)
+    (define-key cider-sub-map (kbd "r") #'cider-refresh)
+    (define-key cider-sub-map (kbd "R") #'cider-restart)
+    (define-key cider-sub-map (kbd "c") #'cider-connect)
+    (define-key cider-sub-map (kbd "q") #'cider-quit)
+    (define-key cider-sub-map (kbd "v") #'cider-jump-to-var)
+    (define-key cider-sub-map (kbd "t") #'cider-toggle-trace-var)
+    (define-key cider-sub-map (kbd "f") #'cb-next-cider-window) ;; f=focus.
+    (define-key cider-sub-map (kbd "e") #'cider-jump-to-compilation-error) ;;
+    cider-sub-map))
+
+(defvar buffer-sub-map
+  (let (buffer-sub-map)
+    (define-prefix-command 'buffer-sub-map)
+    (define-key buffer-sub-map (kbd "w") #'save-buffer)
+    (define-key buffer-sub-map (kbd "W") #'save-and-load)
+    (define-key buffer-sub-map (kbd "k") #'kill-this-buffer)
+    (define-key buffer-sub-map (kbd "b") #'projectile-switch-to-buffer)
+    (define-key buffer-sub-map (kbd "o") #'projectile-switch-to-buffer-other-window)
+    (define-key buffer-sub-map (kbd "p") #'projectile-ibuffer)
+    (define-key buffer-sub-map (kbd "B") #'ibuffer)
+    ;; cycle buffers
+    (define-key buffer-sub-map (kbd "f") #'cb-next-files-buffer)
+    (define-key buffer-sub-map (kbd "n") #'cb-next-buffer-contextual)
+    (define-key buffer-sub-map (kbd "N") #'cb-next-all-buffer)
+    (define-key buffer-sub-map (kbd "*") #'cb-next-emacs-buffer)
+    buffer-sub-map))
+
 (evil-leader/set-key
-  "d" 'cider-doc
-  "G" 'cider-grimoire-eww
-  "c" 'cider-apropos
-  "D" 'cider-apropos-documentation
+  "d" 'cider-doc-map  ;; this is defined by cider but we get ,d ... instead of C-c C-d ...
+  ;; a - cider apropos
+  ;; A - cider apropos doc
+  ;; d - doc
+  ;; g - grimoire
+  ;; h - grimoire web
+  ;; j - javadoc
+  "r" 'cider-sub-map  ;; c is used by nerd-commenter. r = repl.
+  "b" 'buffer-sub-map  ;; manage buffers.
 
   "w" 'save-buffer
-  "t" 'find-next-tag-without-ns
+  "W" 'save-and-load ;; save buffer and load into cider.
+
+  ;;; Finding
+  "t" 'find-next-tag-without-ns  ;; ctags
+  "v" 'cider-jump-to-var
   "a" 'projectile-ag
-  "g" 'magit-status
+  "s" 'evil-ace-jump-word-mode  ;;  ace jump search
+
+  "g" 'magit-status  ;; magit is git.
   "j" 'dired-jump
 
   "f" 'ido-find-file
-  "F" 'cb-next-files-buffer
-
   "O" 'ido-find-file-other-window
-  "o" 'projectile-switch-to-buffer-other-window
-  "b" 'projectile-switch-to-buffer
 
+  "o" 'projectile-switch-to-buffer-other-window
+  "p" 'projectile-switch-to-buffer
   "P" 'projectile-ibuffer
   "B" 'ibuffer
 
   "x" 'smex  ;; M-x.
   "k" 'kill-this-buffer
   "q" 'kill-this-buffer
+
   "S" 'cb-next-shell-window
-  "s" 'evil-ace-jump-word-mode
-  "W" 'save-and-load ;; save buffer and load into cider.
+
+  "F" 'cb-next-files-buffer
+  "*" 'cb-next-emacs-buffer
   "n" 'cb-next-buffer-contextual
   "N" 'cb-next-all-buffer
 
   "e" 'next-error
   "E" 'next-error
 
-  "r" 'cb-next-cider-window ;; cider connect or focus on cider window.
-  "R" 'cider-restart
-  "C" 'cider-connect
-  "Q" 'cider-quit
   "V" 'undo-tree-visualize
-  "v" 'cider-jump-to-var
 
   "0" 'delete-window
   "1" 'delete-other-windows
