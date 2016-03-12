877b99cb54328750969686d477b7ddac6ee4e446
EricGebhart <e.a.gebhart@gmail.com>
2016-03-11 23:39:32 -0500

    Added clj-refactor and language sub menus.

--- emacs24/config/evil-leader-conf.el
+++ emacs24/config/evil-leader-conf.el
@@ -1,10 +1,22 @@
+;;; package --- evil-leader-conf
+;;; Commentary:
+
 ;; evil-leader configuration
 ;; set the leader key to comma
-(setq evil-leader/in-all-states t)
-(require 'evil-leader)
 
+;;; Code:
+
+(require 'evil-leader)
 (require 'cider-doc)
 (require 'clj-refactor)
+
+;; (require 'google-translate)
+;; (require 'ispell)
+;; (require 'flyspell)
+;; (require 'langtool)
+
+
+(setq evil-leader/in-all-states t)
 
 (global-evil-leader-mode)
 
@@ -46,7 +58,7 @@
     (define-key cider-sub-map (kbd "c") #'cider-connect)
     (define-key cider-sub-map (kbd "j") #'cider-jack-in)
     (define-key cider-sub-map (kbd "q") #'cider-quit)
-    (define-key cider-sub-map (kbd "v") #'cider-jump-to-var)
+    (define-key cider-sub-map (kbd "v") #'cider-find-var)
     (define-key cider-sub-map (kbd "t") #'cider-toggle-trace-var)
     (define-key cider-sub-map (kbd "f") #'cb-next-cider-window) ;; f=focus.
     (define-key cider-sub-map (kbd "e") #'cider-jump-to-compilation-error) ;;
@@ -78,8 +90,90 @@
     (define-key yasnippet-sub-map (kbd "e") #'yas-expand-snippet)
     yasnippet-sub-map))
 
-;; M-x describe-mode will help a lot here.
 
+(defvar language-sub-map
+  (let (language-sub-map)
+    ;;; Ispell - fly spell.
+    (define-prefix-command 'language-sub-map)
+    (define-key language-sub-map (kbd "s") #'ispell-word)
+    (define-key language-sub-map (kbd "S") #'flyspell-check-next-highlighted-word)
+
+    ;;; Lang Tool
+    (define-key language-sub-map (kbd "C") #'langtool-check-buffer)
+    (define-key language-sub-map (kbd "c") #'langtool-check)
+    (define-key language-sub-map (kbd "b") #'langtool-correct-buffer)
+    (define-key language-sub-map (kbd "m") #'langtool-show-message-at-point)
+    (define-key language-sub-map (kbd "l") #'langtool-switch-default-language)
+    (define-key language-sub-map (kbd "d") #'langtool-check-done)
+  ;;; get smooth Google translate for cusor point.
+  ;;; - highlight what you want if more than one word.
+    (define-key language-sub-map (kbd "t") #'google-translate-smooth-translate-sentence-at-point)
+    (define-key language-sub-map (kbd "T") #'google-translate-smooth-translate)
+    (define-key language-sub-map (kbd "i") #'set-input-and-dictionary)
+    (define-key language-sub-map (kbd "D") #'describe-input-method)
+    (define-key language-sub-map (kbd "R") #'reset-language-environment)
+    ;;(define-key language-sub-map (kbd "I") #'set-input-method)  ;;  French-prefix, etc.
+    (define-key language-sub-map (kbd "f") #'french-input-dict)  ;;  French-prefix, etc.
+    (define-key language-sub-map (kbd "e") #'english-input-dict)  ;;  French-prefix, etc.
+    (define-key language-sub-map (kbd "I") #'toggle-input-method) ;; C-\ by default.
+    language-sub-map))
+
+;; doing it the hard way.
+(defvar clj-refactor-sub-map
+  (let (clj-refactor-sub-map)
+    (define-prefix-command 'clj-refactor-sub-map)
+    (define-key clj-refactor-sub-map (kbd "ad") #'cljr-add-declaration)
+    (define-key clj-refactor-sub-map (kbd "ai") #'cljr-add-import-to-ns)
+    (define-key clj-refactor-sub-map (kbd "am") #'cljr-add-missing-libspec)
+    (define-key clj-refactor-sub-map (kbd "ap") #'cljr-add-project-dependency)
+    (define-key clj-refactor-sub-map (kbd "ar") #'cljr-add-require-to-ns)
+    (define-key clj-refactor-sub-map (kbd "au") #'cljr-add-use-to-ns)
+    (define-key clj-refactor-sub-map (kbd "cc") #'cljr-cycle-coll)
+    (define-key clj-refactor-sub-map (kbd "ci") #'cljr-cycle-if)
+    (define-key clj-refactor-sub-map (kbd "cn") #'cljr-clean-ns)
+    (define-key clj-refactor-sub-map (kbd "cp") #'cljr-cycle-privacy)
+    (define-key clj-refactor-sub-map (kbd "cs") #'cljr-change-function-signature)
+    (define-key clj-refactor-sub-map (kbd "ct") #'cljr-cycle-thread)
+    (define-key clj-refactor-sub-map (kbd "dk") #'cljr-destructure-keys)
+    (define-key clj-refactor-sub-map (kbd "ef") #'cljr-extract-function)
+    (define-key clj-refactor-sub-map (kbd "ec") #'cljr-extract-constant)
+    (define-key clj-refactor-sub-map (kbd "ed") #'cljr-extract-def)
+    (define-key clj-refactor-sub-map (kbd "ef") #'cljr-extract-function)
+    (define-key clj-refactor-sub-map (kbd "el") #'cljr-expand-let)
+    (define-key clj-refactor-sub-map (kbd "fe") #'cljr-create-fn-from-example)
+    (define-key clj-refactor-sub-map (kbd "fu") #'cljr-find-usages)
+    (define-key clj-refactor-sub-map (kbd "hd") #'cljr-hotload-dependency)
+    (define-key clj-refactor-sub-map (kbd "il") #'cljr-introduce-let)
+    (define-key clj-refactor-sub-map (kbd "is") #'cljr-inline-symbol)
+    (define-key clj-refactor-sub-map (kbd "mf") #'cljr-move-form)
+    (define-key clj-refactor-sub-map (kbd "ml") #'cljr-move-to-let)
+    (define-key clj-refactor-sub-map (kbd "pc") #'cljr-project-clean)
+    (define-key clj-refactor-sub-map (kbd "pf") #'cljr-promote-function)
+    (define-key clj-refactor-sub-map (kbd "rf") #'cljr-rename-file-or-dir)
+    (define-key clj-refactor-sub-map (kbd "rf") #'cljr-rename-file)
+    (define-key clj-refactor-sub-map (kbd "rl") #'cljr-remove-let)
+    (define-key clj-refactor-sub-map (kbd "rr") #'cljr-remove-unused-requires)
+    (define-key clj-refactor-sub-map (kbd "rs") #'cljr-rename-symbol)
+    (define-key clj-refactor-sub-map (kbd "ru") #'cljr-replace-use)
+    (define-key clj-refactor-sub-map (kbd "sn") #'cljr-sort-ns)
+    (define-key clj-refactor-sub-map (kbd "sc") #'cljr-show-changelog)
+    (define-key clj-refactor-sub-map (kbd "sp") #'cljr-sort-project-dependencies)
+    (define-key clj-refactor-sub-map (kbd "sr") #'cljr-stop-referring)
+    (define-key clj-refactor-sub-map (kbd "tf") #'cljr-thread-first-all)
+    (define-key clj-refactor-sub-map (kbd "th") #'cljr-thread)
+    (define-key clj-refactor-sub-map (kbd "tl") #'cljr-thread-last-all)
+    (define-key clj-refactor-sub-map (kbd "ua") #'cljr-unwind-all)
+    (define-key clj-refactor-sub-map (kbd "up") #'cljr-update-project-dependencies)
+    (define-key clj-refactor-sub-map (kbd "uw") #'cljr-unwind)
+    (define-key clj-refactor-sub-map (kbd "?") #'cljr-describe-refactoring)
+    (define-key clj-refactor-sub-map (kbd "hh") #'hydra-cljr-help-menu/body)
+    (define-key clj-refactor-sub-map (kbd "hn") #'hydra-cljr-ns-menu/body)
+    (define-key clj-refactor-sub-map (kbd "hc") #'hydra-cljr-code-menu/body)
+    (define-key clj-refactor-sub-map (kbd "hp") #'hydra-cljr-project-menu/body)
+    (define-key clj-refactor-sub-map (kbd "hs") #'hydra-cljr-toplevel-form-menu/body)
+    clj-refactor-sub-map))
+
+;; M-x describe-mode will help a lot here.
 (evil-leader/set-key
   "D" 'describe-mode
   "d" 'cider-doc-map  ;; this is defined by cider but we get ,d ... instead of C-c C-d ...
@@ -88,21 +182,25 @@
   ;; d - doc
   ;; g - grimoire
   ;; h - grimoire web
-  ;; j - javadoc
+  ;; j - Java doc
   "r" 'cider-sub-map  ;; c is used by nerd-commenter. r = repl.
   "b" 'buffer-sub-map  ;; manage buffers.
 
+
   ;; Refactoring, code snippets etc.
   ;; clojure refactor key bindings, Also mapped to C-c C-m.
-  "R" 'cljr--all-helpers ;; clj-refactor.
+  "R" 'clj-refactor-sub-map ;;'cljr--all-helpers ;; clj-refactor.
   "y" 'yasnippet-sub-map
 
   "w" 'save-buffer
   "W" 'save-and-load ;; save buffer and load into cider.
 
+  "l" 'language-sub-map
+
+
   ;;; Finding
   "t" 'find-tag-without-ns  ;; ctags
-  "v" 'cider-jump-to-var
+  "v" 'cider-find-var
   "a" 'projectile-ag
   "s" 'evil-ace-jump-word-mode  ;;  ace jump search
 
@@ -138,5 +236,9 @@
   "2" 'split-window-below
   "3" 'split-window-right
 
-  "," 'evilnc-comment-operator
-  )
+  "," 'evilnc-comment-operator)
+
+
+
+(provide 'evil-leader-conf)
+;;; evil-leader-conf.el ends here.
