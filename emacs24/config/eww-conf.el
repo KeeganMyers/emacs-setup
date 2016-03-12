1f469621426215f1d51cc9ce41677f69945f9a9b
EricGebhart <e.a.gebhart@gmail.com>
2016-03-11 23:34:30 -0500

    add larger contrast and luminosity for eww.  Email looked awful.

---
+++ emacs24/config/eww-conf.el
@@ -0,0 +1,6 @@
+(require 'eww)
+
+;;shr-color-visible-distance-min and shr-color-visible-luminance-min to get a better
+
+(setq shr-color-visible-distance-min 20) ;; default is 5
+(setq shr-color-visible-luminance-min 80) ;; default is 40
