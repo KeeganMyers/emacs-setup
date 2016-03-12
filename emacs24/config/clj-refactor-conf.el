7aac3660e97f11962a67d542857ce0abb8d24883
EricGebhart <e.a.gebhart@gmail.com>
2016-03-11 23:29:18 -0500

    fixing it up.

--- emacs24/config/clj-refactor-conf.el
+++ emacs24/config/clj-refactor-conf.el
@@ -1,6 +1,15 @@
+;;; package --- clj-refactor
+;;; Commentary:
+
 (require 'clj-refactor)
 
-(add-hook 'clojure-mode-hook (lambda ()
-                               (clj-refactor-mode 1)
-                               (cljr-add-keybindings-with-prefix "C-c C-m")
-                               ))
+;;; Code:
+(defun my-clj-refactor-clojure-mode-hook ()
+  "Set up the refactor hook."
+  (clj-refactor-mode 1)
+  (yas-minor-mode 1) ; for adding require/use/import statements
+  ;; This choice of keybinding leaves cider-macroexpand-1 unbound
+  (cljr-add-keybindings-with-prefix "C-c C-m"))
+
+(add-hook 'clojure-mode-hook #'my-clj-refactor-clojure-mode-hook)
+;;; clj-refactor-conf.el ends here
