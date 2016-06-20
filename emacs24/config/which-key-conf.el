a61d3ae920f86c15da4680ef752c2bc8a02a2472
EricGebhart <e.a.gebhart@gmail.com>
2016-06-20 14:53:06 -0400

    Added which-key

---
+++ emacs24/config/which-key-conf.el
@@ -0,0 +1,38 @@
+(require 'which-key)
+
+(which-key-mode)
+
+;;(which-key-setup-side-window-right)
+;;(which-key-setup-minibuffer)
+
+
+;; Additional settings
+
+;;(setq which-key-popup-type 'minibuffer)
+
+
+;;(setq which-key-popup-type 'side-window)
+
+;; location of which-key window. valid values: top, bottom, left, right,
+;; or a list of any of the two. If it's a list, which-key will always try
+;; the first location first. It will go to the second location if there is
+;; not enough room to display any keys in the first location
+;;(setq which-key-side-window-location 'bottom)
+
+;; max width of which-key window, when displayed at left or right.
+;; valid values: number of columns (integer), or percentage out of current
+;; frame's width (float larger than 0 and smaller than 1)
+;;(setq which-key-side-window-max-width 0.33)
+
+;; max height of which-key window, when displayed at top or bottom.
+;; valid values: number of lines (integer), or percentage out of current
+;; frame's height (float larger than 0 and smaller than 1)
+;;(setq which-key-side-window-max-height 0.25)
+
+
+;;(setq which-key-popup-type 'frame)
+;; max width of which-key frame: number of columns (an integer)
+;;(setq which-key-frame-max-width 60)
+
+;; max height of which-key frame: number of lines (an integer)
+;;(setq which-key-frame-max-height 20)
